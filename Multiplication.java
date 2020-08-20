1967fcbf4587e920f45ef894e2f7e2ff7d923af6
thienchanvota1312 <37357766+thienchanvota1312@users.noreply.github.com>
2020-08-20 19:13:29 +0700

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+package Tuan_5;
+
+public class Multiplication extends BinaryExpression{
+    public Multiplication(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    public String toString() {
+        return "(" + left.toString() + " * " + right.toString() + ")";
+    }
+    
+    public double evaluate(){
+        return left.evaluate() * right.evaluate();
+    }
+}
