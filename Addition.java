6309b04c067d11ed9623095e75c4a2b70db5735b
thienchanvota1312 <37357766+thienchanvota1312@users.noreply.github.com>
2020-08-20 19:03:33 +0700

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package Tuan_5;
+
+public class Addition extends BinaryExpression {
+    public Addition(Expression left, Expression right){
+        super(left, right);
+    }    
+    @Override
+    public String toString() {
+        return ;
+    }
+    @Override
+    public double evaluate() {
+        return ;
+    }
+}
