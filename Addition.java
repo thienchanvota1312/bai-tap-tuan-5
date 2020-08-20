c6a1d321f2987c48ad14db2199a9d0044e2b91b5
thienchanvota1312 <37357766+thienchanvota1312@users.noreply.github.com>
2020-08-20 19:05:54 +0700

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -4,11 +4,11 @@ public class Addition extends BinaryExpression {
     public Addition(Expression left, Expression right){
         super(left, right);
     }    
-    @Override
+    
     public String toString() {
         return ;
     }
-    @Override
+    
     public double evaluate() {
         return ;
     }
